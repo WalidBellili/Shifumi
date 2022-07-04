1849805bc06ffaf6185d5988a223284de012721f
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-07-04 15:52:49 +0200

    setInterval works mais un peu trop

--- index.js
+++ index.js
@@ -13,7 +13,25 @@ arrowDown.addEventListener("click", (e) => {
     console.log(e);
 })
 btn.addEventListener('click', (e) => {
-    
+    const imgDisplay = () => {
+        const randomImg = Math.ceil(Math.random()*3) 
+       console.log(randomImg);
+       if (randomImg === 1) {
+           stone.innerHTML = `
+           <img src="./2-feuille.jpg" height="250" width="250">
+           `
+       } else if(randomImg===2) {
+           stone.innerHTML = `
+           <img src="./3-ciseaux.jpg" height="250" width="250">
+           `
+       } else {
+           stone.innerHTML = `
+           <img src="./1-pierre.jpg" height="250" width="250">
+           `
+       }
+       }
+       imgDisplay()
+       setInterval(imgDisplay, 1000)
 })
 
 // const setInter = () => {
@@ -38,22 +56,3 @@ btn.addEventListener('click', (e) => {
 //     `
 // }
 
-const imgDisplay = () => {
- const randomImg = Math.ceil(Math.random()*3) 
-console.log(randomImg);
-if (randomImg === 1) {
-    stone.innerHTML = `
-    <img src="./2-feuille.jpg" height="250" width="250">
-    `
-} else if(randomImg===2) {
-    stone.innerHTML = `
-    <img src="./3-ciseaux.jpg" height="250" width="250">
-    `
-} else {
-    stone.innerHTML = `
-    <img src="./1-pierre.jpg" height="250" width="250">
-    `
-}
-}
-imgDisplay()
-setInterval()
