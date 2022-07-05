5a40eaa8214ab0323aef89adc9add4f8366fba8e
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-07-05 22:53:18 +0200

    A refaire

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const algoGame = () => {
       const player = btn[i].innerHTML;
       const bot = btn[Math.floor(Math.random() * btn.length)].innerHTML;
       let result = "";
+      let scorePlayer = 0;
+      let scoreBot = 0;
 
       if (player === bot) {
         result = "Egalité";
@@ -19,13 +21,32 @@ const algoGame = () => {
       } else {
         result = "Bender a gagné";
       }
+      const score = () => {
+        if (player > bot) {
+          scorePlayer += 1;
+          console.log(scorePlayer);
+        } else if (player < bot) {
+          scoreBot += 1;
+          console.log(scoreBot);
+        } else if (player === bot) {
+          console.log("égalité");
+        }
+      };
+      score();
+
       results.innerHTML = `
           <article>
           <h2>Bender : ${bot}</h2>
+          <img
+          src="./bender-tordeur-rodriguez-250.jpg"
+          alt=""
+          width="100"
+          height="100"
+        />
         </article>
-      
         <article>
           <h2>Vous : ${player}</h2>
+
         </article>
         ${result}
           `;
