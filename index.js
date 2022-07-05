7bd140c75fb230a45409fdeb375236570668a887
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-07-05 21:42:19 +0200

    responsive nearly done

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const btn = document.querySelectorAll("button");
 const results = document.querySelector(".result");
 
-for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", () => {
-    const player = btn[i].innerHTML;
-    const bot = btn[Math.floor(Math.random() * btn.length)].innerHTML;
-    let result = "";
+const algoGame = () => {
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", () => {
+      const player = btn[i].innerHTML;
+      const bot = btn[Math.floor(Math.random() * btn.length)].innerHTML;
+      let result = "";
 
-    if (player === bot) {
-      result = "Egalité";
-    } else if (
-      (player === "Pierre" && bot === "Ciseaux") ||
-      (player === "Feuille" && bot === "Pierre") ||
-      (player === "Ciseaux" && bot === "Feuille")
-    ) {
-      result = "Win";
-    } else {
-      result = "Loose";
-    }
-    results.innerHTML = `
-    <article>
-    <h2>Bender : ${bot}</h2>
-  </article>
-  <button>Play</button>
-  <article>
-    <h2>Vous : ${player}</h2>
-  </article>
-  ${result}
-    `;
-  });
-}
+      if (player === bot) {
+        result = "Egalité";
+      } else if (
+        (player === "Pierre" && bot === "Ciseaux") ||
+        (player === "Feuille" && bot === "Pierre") ||
+        (player === "Ciseaux" && bot === "Feuille")
+      ) {
+        result = "Vous avez gagné";
+      } else {
+        result = "Bender a gagné";
+      }
+      results.innerHTML = `
+          <article>
+          <h2>Bender : ${bot}</h2>
+        </article>
+      
+        <article>
+          <h2>Vous : ${player}</h2>
+        </article>
+        ${result}
+          `;
+    });
+  }
+};
+algoGame();
